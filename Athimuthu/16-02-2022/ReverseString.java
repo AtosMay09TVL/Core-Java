cc6c5de9191219d4b1fdb7a913dfc3b55fb65ce0
AathiMuthu <106526324+AathiMuthu@users.noreply.github.com>
2022-06-01 15:02:54 +0530

    ReverseString

---
+++ Athimuthu/16-02-2022/ReverseString.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class Reverse{
+  
+public static void main(String[] args) {
+  String name="Aathimuthu" Sp=" ";
+  char A;
+  System.out.println(" The word is "+Name);
+  for(int i=0;i<Name.length();i++)
+  {
+    A=Name.CharAt(i);
+    Sp=A+Sp;
+  }
+    System.out.println(" The reversed String is "+sp);
+}
+}
